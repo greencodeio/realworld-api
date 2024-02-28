57a9d6d6a8baad271a3c21fd87a0daed248bb667
greencodeio <igormaslo.a@gmail.com>
2024-02-28 19:35:49 +0200

    Fix typeorm ds

--- src/typeorm-ds.ts
+++ src/typeorm-ds.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '7355608',
-    database: 'real-world',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/migrations/*.js'],
     synchronize: false,
